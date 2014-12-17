f5043740a5c776465569c23b1bc360db373843f1
Wieber Juergen <Juergen.Wieber@de.yokogawa.com>
2014-12-17 20:32:25 +0100

    Projekt Backgroundworker hinzugefuegt
    Signed-off-by: Wieber Juergen <Juergen.Wieber@de.yokogawa.com>

---
+++ PlayGround/BackgroundWorkerSample/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerSample
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker m_oWorker;
+        public Form1()
+        {
+            InitializeComponent();
+            m_oWorker = new BackgroundWorker();
+            m_oWorker.DoWork += new DoWorkEventHandler(m_oWorker_DoWork);
+            m_oWorker.ProgressChanged += new ProgressChangedEventHandler(m_oWorker_ProgressChanged);
+            m_oWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(m_oWorker_RunWorkerCompleted);
+            m_oWorker.WorkerReportsProgress = true;
+            m_oWorker.WorkerSupportsCancellation = true;
+        }
+        void m_oWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                lblStatus.Text = "Task Cancelled.";
+            }
+            else if (e.Error != null)
+            {
+                lblStatus.Text = "Error while performing background operation.";
+            }
+            else
+            {
+                lblStatus.Text = "Task Completed...";
+            }
+            btnStartAsyncOperation.Enabled = true & !this.checkBox1.Checked;
+            btnCancel.Enabled = false;
+            this.timer1.Enabled = this.checkBox1.Checked;
+        }
+        void m_oWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lblStatus.Text = "Processing......" + progressBar1.Value.ToString() + "%";
+        }
+        void m_oWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                m_oWorker.ReportProgress(i);
+                if (m_oWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    m_oWorker.ReportProgress(0);
+                    return;
+                }
+            }
+            m_oWorker.ReportProgress(100);
+        }
+        private void btnStartAsyncOperation_Click(object sender, EventArgs e)
+        {
+            btnStartAsyncOperation.Enabled  = false;
+            btnCancel.Enabled               = true;
+            m_oWorker.RunWorkerAsync();
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (m_oWorker.IsBusy)
+            {
+                this.checkBox1.Checked = false;
+                m_oWorker.CancelAsync();
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(this.btnStartAsyncOperation.Enabled)
+            {
+                this.timer1.Enabled = true;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = false;
+            this.btnStartAsyncOperation_Click(sender, e);
+        }
+    }
+}
