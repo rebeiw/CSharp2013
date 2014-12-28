94afd4ea1804f558c71c6422d1979d4ec54a20e7
Wieber Juergen <jwieber@t-online.de>
2014-12-29 00:43:34 +0100

    StyleCop hinzugefuegt
    Signed-off-by: Wieber Juergen <jwieber@t-online.de>

---
+++ PlayGround/StyleCopDemo/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StyleCop
+{
+    public class TestClass
+    {
+        public string myProperty { get; set; }
+    }
+}
