bf6fc41b8acf7252230e099ef049ca542e72dcf3
Wieber Juergen <jwieber@t-online.de>
2015-01-24 11:00:53 +0100

    Projekt Report hinzugefuegt
    Signed-off-by: Wieber Juergen <jwieber@t-online.de>

---
+++ Projekte/Report/CreateDb.bat
@@ -0,0 +1,6 @@
+sqlite3.exe daten.db ".tables"
+
+
+sqlite3.exe daten.db ".read SetupSql.sql"
+
+rem sqlite3.exe daten.db ".read insert.sql"
---
+++ Projekte/Report/Form1.cs
@@ -0,0 +1,41 @@
+﻿using Helper;
+using System;
+using System.Windows.Forms;
+
+
+namespace Report
+{
+    public partial class Form1 : Form
+    {
+
+        ClsPDF m_ClsPdf;
+        public Form1()
+        {
+            InitializeComponent();
+            this.m_ClsPdf = new ClsPDF();
+            this.m_ClsPdf.AddPage();
+
+            for (int i = 0; i < 10;i++ )
+            {
+                this.m_ClsPdf.DrawString("Wieber Juergen " + i.ToString() , 0, i);
+            }
+            this.m_ClsPdf.AddPage();
+            for (int i = 0; i < 10; i++)
+            {
+                this.m_ClsPdf.DrawString("Wieber Christine " + i.ToString(), 5, i);
+            }
+
+            this.m_ClsPdf.SetPage(0);
+            for (int i = 0; i < 10; i++)
+            {
+                this.m_ClsPdf.DrawString("Wieber Christine " + i.ToString(), 5, i+10);
+            }
+            this.m_ClsPdf.ShowDocument();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
